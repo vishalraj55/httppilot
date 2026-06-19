9a14051faa5243d3f95c9de30a2488fe5a55c69c
Vishal <gamingdark340@gmail.com>
2026-06-19 12:41:20 +0530

    Update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,9 @@ async function bootstrap() {
   });
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://httppilot.vercel.app'],
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 }
