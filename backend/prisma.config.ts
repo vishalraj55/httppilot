e9032e37858a818f1e3928307faa1c6229169b8c
Vishal <gamingdark340@gmail.com>
2026-06-19 12:08:25 +0530

    Update

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import path from 'path';
 
 export default defineConfig({
-  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
+  schema: path.join(__dirname, 'src', 'prisma', 'schema.prisma'),
   datasource: {
     url: process.env.DATABASE_URL!,
   },
